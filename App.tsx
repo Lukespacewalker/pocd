2d299ad5b11db78e084424fcddaa22c14fe92907
Lukespacewalker <suttisak.lukesp@outlook.com>
2023-02-07 11:52:06 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -7,7 +7,8 @@ import {
   UserOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import { Breadcrumb, Layout, Menu, theme,  Space, Table, Tag  } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -41,6 +42,89 @@ const items: MenuItem[] = [
   ]),
   getItem('Files', '9', <FileOutlined />),
 ];
+
+/* TABLE */
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <React.Fragment>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </React.Fragment>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
 
 const App: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -85,6 +169,7 @@ const App: React.FC = () => {
           >
             Bill is a cat.
           </div>
+          <Table columns={columns} dataSource={data} />
         </Content>
         <Footer style={{ textAlign: 'center' }}>
           Ant Design ©2023 Created by Ant UED
